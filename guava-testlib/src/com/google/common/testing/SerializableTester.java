4336326e7b9b31f4b3fa265580b45820dddb1bbe
Éamonn McManus <emcmanus@google.com>
2021-12-13 14:48:55 -0800

    Remove `@Beta` from a number of `com.google.common.testing` classes.
    RELNOTES=Several classes in `com.google.common.testing` that were previously marked `@Beta` are now fully supported.
    PiperOrigin-RevId: 416139593

--- guava-testlib/src/com/google/common/testing/SerializableTester.java
+++ guava-testlib/src/com/google/common/testing/SerializableTester.java
@@ -16,7 +16,6 @@
 
 package com.google.common.testing;
 
-import com.google.common.annotations.Beta;
 import com.google.common.annotations.GwtCompatible;
 import junit.framework.Assert;
 import junit.framework.AssertionFailedError;
@@ -32,7 +31,6 @@ import junit.framework.AssertionFailedError;
  * @author Mike Bostock
  * @since 10.0
  */
-@Beta
 @GwtCompatible // but no-op!
 public final class SerializableTester {
   private SerializableTester() {}
